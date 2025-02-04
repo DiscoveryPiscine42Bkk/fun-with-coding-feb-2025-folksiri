149381f325e964045a9176539a62082ca0ec7e88
folksiri <thanakrit.sirimogkolwattana@gmail.com>
2025-02-04 18:47:06 +0700

    1

--- discovery_piscine/cell00/ex04/argv.sh
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]; then
     echo "No argument supplied"
 else
-    if [ "$1" ]; then echo $1; fi
-    if [ "$2" ]; then echo $2; fi
-    if [ "$3" ]; then echo $3; fi
+    [ "$1" ] && echo "$1"
+    [ "$2" ] && echo "$2"
+    [ "$3" ] && echo "$3"
 fi
